f0e41c909815d0969ccd1059ce4be1c1a5e2d328
Rohan <rohanshikhare410@gmail.com>
2024-09-09 11:41:07 +0530

    made the bet button blue [ cheacking for vercel]

--- src/SendTokens.jsx
+++ src/SendTokens.jsx
@@ -28,6 +28,6 @@ export function SendTokens({bet,setBet}) {
     }
     return <div>
         {/* <input onChange={handleAmount} id="amount" type="text" placeholder="Amount" /> */}
-        <button className="mt-5 bg-green-500 text-white px-4 py-2 rounded transition duration-150 active:bg-green-500" onClick={sendTokens}>BET</button>
+        <button className="mt-5 bg-blue-500 text-white px-4 py-2 rounded transition duration-150 active:bg-green-500" onClick={sendTokens}>BET</button>
     </div>
 }
