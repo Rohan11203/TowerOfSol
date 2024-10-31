21cca3f8ab57eb3b3c74c3c28a8b415edb2d4d9a
Rohan <rohanshikhare410@gmail.com>
2024-10-31 11:53:06 +0530

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { Analytics } from "@vercel/analytics/react"
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
